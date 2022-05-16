0287773c5e63ea537db21bcb9654ffa7d526e926
辣橙 <2625835752@qq.com>
2022-05-17 00:22:58 +0800

    Delete HelloWorld.vue

--- components.d.ts
+++ components.d.ts
@@ -13,7 +13,6 @@ declare module '@vue/runtime-core' {
     ElFooter: typeof import('element-plus/es')['ElFooter']
     FooterArea: typeof import('./src/components/FooterArea.vue')['default']
     FormArea: typeof import('./src/components/FormArea.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     LaminateResult: typeof import('./src/components/result-area/LaminateResult.vue')['default']
     LayerAttributeInput: typeof import('./src/components/LayerAttributeInput.vue')['default']
     LayerResult: typeof import('./src/components/result-area/LayerResult.vue')['default']
