cd7d07c824f45bd94d9d4d3afd5fb83c9f507c0d
辣橙 <2625835752@qq.com>
2022-05-28 00:13:10 +0800

    v3.0.0.beta.1

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import VueLatex from 'vatex'
-import {createPinia} from "pinia";
-
-createApp(App)
-    .use(createPinia())
-    .use(VueLatex)
-    .mount('#app')
