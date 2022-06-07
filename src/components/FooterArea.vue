0ac058616577b12738d17df8e36c2fbafefc47b4
辣橙 <2625835752@qq.com>
2022-06-07 12:07:21 +0800

    docs: optimize docs

--- src/components/FooterArea.vue
+++ src/components/FooterArea.vue
@@ -13,13 +13,23 @@ function open(href: string) {
 <template>
   <donate-dialog v-model="showDonateDialog"></donate-dialog>
   <el-footer>
-    <div>Made with ❤️ by <span @click="open('https://github.com/laorange')" style="cursor: pointer">GitHub@laorange</span></div>
-    <el-button type="success" :icon="Trophy" @click="showDonateDialog=true" size="small">赞赏支持</el-button>
+    <div>源代码: <a href="https://github.com/laorange/LaminateCalculation">GitHub</a> | <a href="https://gitee.com/laorange/LaminateCalculation">Gitee</a></div>
+    <div class="donate-button-area">
+      <div>Developed by <span @click="open('https://github.com/laorange')" style="cursor: pointer">GitHub@laorange</span></div>
+      <el-button type="success" :icon="Trophy" @click="showDonateDialog=true" size="small">赞赏支持</el-button>
+    </div>
   </el-footer>
 </template>
 
 <style scoped>
 .el-footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100px;
+}
+
+.donate-button-area {
   display: flex;
   flex-direction: row;
   justify-items: center;
