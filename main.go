5c1f43db4238e29013455853726a6ef22a900801
辣橙 <2625835752@qq.com>
2022-06-07 11:55:19 +0800

    docs: optimize prompt

--- main.go
+++ main.go
@@ -13,18 +13,21 @@ import (
 var dist embed.FS
 
 func main() {
-	print("这个黑框是复合材料性能计算程序的本地后台服务，请勿在结束使用前关闭它")
-
 	rand.Seed(time.Now().UnixNano())
 	PORT := strconv.Itoa(8000 + rand.Intn(1000)) // 在8000-9000随机挑一个作为端口
+
+	url := `http://127.0.0.1:`+PORT+"/dist/"
+
+	println("这个黑框是复合材料性能计算程序的本地后台服务，请勿在结束使用前关闭它")
+	println("您可以访问 " + url + " 来使用程序")
 
 	http.Handle("/", http.FileServer(http.FS(dist)))
 
 	// windows
-	exec.Command(`cmd`, `/c`, `start`, `http://127.0.0.1:`+PORT+"/dist/").Start()
+	// exec.Command(`cmd`, `/c`, `start`, url).Start()
 
 	// mac
-	// exec.Command(`open`, `http://127.0.0.1:`+PORT+"/dist/").Start()
+	exec.Command(`open`, url).Start()
 
 	http.ListenAndServe(":"+PORT, nil)
 }
